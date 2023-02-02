a606212b6f4effe598916f5322e2527532e39c25
jebbwebb <98522086+jebbwebb@users.noreply.github.com>
2023-02-01 20:44:38 -0600

    finished the pagination of the market component however not quite sure on how to filter search the entire list of items

--- src/components/Market.js
+++ src/components/Market.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { json } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
 
 export default function Market() {
   const [items, setItems] = useState([]);
   const [pageCount, setpageCount] = useState(0);
   const [query, setQuery] = useState('');
+  const [meme, setMeme] = useState([]);
   useEffect(() => {
-    fetch('https://api.tarkov.dev/graphql', {
+    fetchData();
+  }, []);
+
+  const fetchData = async (currentPage) => {
+    const data = fetch('https://api.tarkov.dev/graphql', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -15,26 +20,28 @@ export default function Market() {
       },
       body: JSON.stringify({
         query: `{
-          items(limit: 10 offset:10 ) {
-            id
-            name
-            image512pxLink
-            avg24hPrice
-           wikiLink
-          }
-        }`,
+        items(limit: 10 offset:${
+          (currentPage == null ? 0 : currentPage) * 10
+        } ) {
+          id
+          name
+          image512pxLink
+          avg24hPrice
+         wikiLink
+        }
+      }`,
       }),
     })
       .then((r) => r.json())
       .then((r) => setItems(r.data.items));
-  }, []);
+  };
 
-  const handleSearch = () => {};
-  console.log(items);
-  const filterTest = items.filter((item) =>
-    item.name.toLowerCase().includes('shot')
-  );
-  console.log(query);
+  const handlePageClick = async (data) => {
+    let itemsFromServer = await fetchData(data.selected);
+    console.log(itemsFromServer);
+    setItems(itemsFromServer.data.items);
+  };
+
   const listedItems = items
     .filter((item) => item.name.toLowerCase().includes(query))
     .map((item) => {
@@ -83,6 +90,26 @@ export default function Market() {
       </div>
 
       {listedItems}
+
+      <ReactPaginate
+        previousLabel={'previous'}
+        nextLabel={'next'}
+        breakLabel={'...'}
+        pageCount={10}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={3}
+        onPageChange={handlePageClick}
+        containerClassName={'pagination justify-content-center'}
+        pageClassName={'page-item'}
+        pageLinkClassName={'page-link'}
+        previousClassName={'page-item'}
+        previousLinkClassName={'page-link'}
+        nextClassName={'page-item'}
+        nextLinkClassName={'page-link'}
+        breakClassName={'page-item'}
+        breakLinkClassName={'page-link'}
+        activeClassName={'active'}
+      ></ReactPaginate>
     </>
   );
 }
